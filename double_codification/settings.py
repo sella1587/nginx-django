e583498fc6e862fdd0e71b91f5ad993e5e19ad22
RAZANAJATOVO Henintsoa Cela Fabrice <52450533+sella1587@users.noreply.github.com>
2025-01-21 21:44:04 +0100

    Update settings.py

--- double_codification/settings.py
+++ double_codification/settings.py
@@ -146,8 +146,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT  = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+STATIC_ROOT  = '/app/static/'
 CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
 CRISPY_TEMPLATE_PACK = "tailwind"
 
